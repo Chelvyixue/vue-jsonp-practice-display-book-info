6cf9a6d5251fcc39f6a1cf4a77a66f7faf578c71
clyx <chelvyixue@gmail.com>
2017-04-24 00:26:29 +0800

    Finish id router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Resourse from 'vue-resource'
+import VueJsonp from 'vue-jsonp'
 
-Vue.use(Router);
+Vue.use(Router)
+Vue.use(Resourse)
+Vue.use(VueJsonp)
 
 import Book from './components/Book.vue'
 
 export default new Router({
 	routes: [
 		{
-			path: '/',
-			component: Book
+			path: '/book/:id',
+			component: Book,
+			props: true
 		}
 	]
 });
